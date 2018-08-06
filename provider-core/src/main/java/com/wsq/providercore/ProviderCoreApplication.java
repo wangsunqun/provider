a9db58f7d8790389c3627886d655e020aff6424c
wangsunqun <1805696978@qq.com>
2018-08-06 10:33:19 +0800

    [+]新建模板

---
+++ provider-core/src/main/java/com/wsq/providercore/ProviderCoreApplication.java
@@ -0,0 +1,12 @@
+package com.wsq.providercore;
+
+import com.wsq.common.serviceStandard.ServiceStandard;
+import org.springframework.boot.SpringApplication;
+
+@ServiceStandard
+public class ProviderCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProviderCoreApplication.class, args);
+	}
+}
