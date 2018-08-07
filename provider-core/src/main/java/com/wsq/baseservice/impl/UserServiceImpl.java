ddedb3daef83f09082e28bff0500ab5f3f05d4ea
victor_qian <victor.qian@nakedhub.com>
2018-08-07 11:08:46 +0800

    增加消息调用

--- provider-core/src/main/java/com/wsq/baseservice/impl/UserServiceImpl.java
+++ provider-core/src/main/java/com/wsq/baseservice/impl/UserServiceImpl.java
@@ -25,9 +25,16 @@ public class UserServiceImpl implements UserInterface {
     @Override
     public UserDto getUserById(@RequestParam("id") long id) {
         System.out.println("请求进来了");
+
         UserDto userDto = new UserDto(1, "王孙群");
         cacheService.add("user1", userDto, 60, CacheType.PASSPORT);
         UserDto result = cacheService.get("user1", CacheType.PASSPORT);
+        try {
+
+            producer.sendMsg("test", "VictorTest", "VictorTest", "test1");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return result;
     }
 }
